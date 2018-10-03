cb307d581874fa4c9a81948076c5770196741f41
Gurinov <evgeniy-gurinov@mail.ru>
2018-10-03 12:35:16 +0300

    add tests

--- Tracer/Properties/AssemblyInfo.cs
+++ Tracer/Properties/AssemblyInfo.cs
@@ -17,23 +17,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-<<<<<<< HEAD
-[assembly: Guid("872EFD15-E91F-4700-A0BE-ADE896569509")]
-=======
-[assembly: Guid("C3FC7A72-5514-49CD-9C14-3712804511DF")]
->>>>>>> Initial commit
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
